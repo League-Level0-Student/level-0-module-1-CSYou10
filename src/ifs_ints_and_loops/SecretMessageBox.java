dc6f86f86db41537a9f395fd221715ee623ee3d3
eliyahubowers <league@WTS-IM-6.attlocal.net>
2019-03-11 18:30:33 -0700

    Message BoxComplete. The RiddlerIncomplete

--- src/ifs_ints_and_loops/SecretMessageBox.java
+++ src/ifs_ints_and_loops/SecretMessageBox.java
@@ -1,6 +1,8 @@
 package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 
+import javax.swing.JOptionPane;
+
 //    Level 0
 
 /**
@@ -18,16 +20,25 @@ public class SecretMessageBox {
 	public static void main(String[] args) {
 
 		// 1. Set a password in a String variable
-
+		String r = "Romans828*";
 		// 2. Using a pop-up, ask the first person for a secret message and store it in
+		String input = JOptionPane.showInputDialog("Type in a message.");
 		// a variable
 
 		// 3. Now use a pop-up to tell the NEXT user that they can only see the secret
 		// message
+		String answer = JOptionPane.showInputDialog("Only the can see the message! If you can guess the passcode.");
 		// if they can guess the passcode
 
 		// 4. If their guess matches the password, show them the secret message
-
+		if (answer.equals(r)) {
+			JOptionPane.showMessageDialog(null,""+input );
+			
+		}else {
+		JOptionPane.showMessageDialog(null, "INTRUDER!");	
+			
+		}
 		// 5. If the password does not match, pop-up "INTRUDER!!"
+		
 	}
 }
--- src/ifs_ints_and_loops/TheRiddler.java
+++ src/ifs_ints_and_loops/TheRiddler.java
@@ -2,18 +2,25 @@ package ifs_ints_and_loops;
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
 	public static void main(String[] args) {
 
 		// 1. Make a variable to hold the score
-
+String score;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
-
+String riddle1 = JOptionPane.showInputDialog("What has a head and a tail, but no body?");
+String answer1 = "A coin.";
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if(riddle1.equals(answer1)) {
+	
+	
+	
+}else {
+	
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
 
 		// 6. Add some more riddles
